b9967f317743fd5be69491eebd17f104014732b7
VasilKuzmanov <118282723+VasilKuzmanov@users.noreply.github.com>
2024-09-08 23:43:01 +0300

    Update multi-channel-notification.jsx

--- multi-channel-notification.jsx
+++ multi-channel-notification.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-
-
+ 
+ 
 const NotificationDatePicker = () => {
   
   const [selectedDays, setSelectedDays] = useState({
@@ -11,40 +11,57 @@ const NotificationDatePicker = () => {
     appFromDay: undefined,
     appToDay: undefined,
   });
-
-
+ 
+ 
   const getAllSelectedDays = () => {
     return Object.values(selectedDays).filter(day => day !== undefined);
   };
-
-
+ 
+ 
   const getValidOptions = (type) => {
     const allSelectedDays = getAllSelectedDays();
-
-    if (type === 'email') {
+ 
+    if (type === 'emailFrom') {
       const phoneFromDay = selectedDays.phoneFromDay;
-      const phoneToDay = selectedDays.phoneToDay;
-      return [1, 2, 3, 4, 5, 6, 7]
+      const emailToDay = selectedDays.emailToDay;
+      return [2, 3, 4, 5, 6, 7]
         .filter(day => !allSelectedDays.includes(day))
         .filter(day => phoneFromDay ? day < phoneFromDay : true)
-        .filter(day => phoneToDay ? day < phoneToDay : true);
+        .filter(day => emailToDay ? emailToDay - day >= 2 : true);
     }
 
-
-    if (type === 'phone') {
+    if (type === 'emailTo') {
+      const phoneToDay = selectedDays.phoneToDay;
       const emailFromDay = selectedDays.emailFromDay;
-      const emailToDay = selectedDays.emailToDay;
-      return [1, 2, 3, 4, 5, 6, 7]
+      return [4, 5, 6, 7]
+        .filter(day => !allSelectedDays.includes(day))
+        .filter(day => phoneToDay ? day < phoneToDay : true)
+        .filter(day => emailFromDay ? day - emailFromDay >= 2 : true);
+    }
+ 
+    if (type === 'phoneFrom') {
+      const emailFromDay = selectedDays.emailFromDay;
+      const phoneToDay = selectedDays.phoneToDay;
+      return [3, 4, 5, 6, 7]
         .filter(day => !allSelectedDays.includes(day))
         .filter(day => emailFromDay ? day > emailFromDay : true)
-        .filter(day => emailToDay ? day > emailToDay : true);
+        .filter(day => phoneToDay ? phoneToDay - day >= 2 : true);
     }
 
-    return [1, 2, 3, 4, 5, 6, 7]
+    if (type === 'phoneTo') {
+      const emailToDay = selectedDays.emailToDay;
+      const phoneFromDay = selectedDays.phoneFromDay;
+      return [5, 6, 7]
+        .filter(day => !allSelectedDays.includes(day))
+        .filter(day => emailToDay ? day > emailToDay : true)
+        .filter(day => phoneFromDay ? day - phoneFromDay >= 2 : true);
+    }
+ 
+    return [2, 3, 4, 5, 6, 7]
       .filter(day => !allSelectedDays.includes(day));
   };
-
-
+ 
+ 
   const handleDayChange = (e) => {
     const { id, value } = e.target;
     setSelectedDays(prevState => ({
@@ -52,8 +69,8 @@ const NotificationDatePicker = () => {
       [id]: value === "" ? undefined : Number(value),
     }));
   };
-
-
+ 
+ 
   const renderOptions = (type) => {
     return getValidOptions(type).map((day, index) => {
       return (
@@ -64,7 +81,7 @@ const NotificationDatePicker = () => {
     });
   };
   
-
+ 
   return (
     <div>
       <h2>Телефонно обаждане</h2>
@@ -72,7 +89,7 @@ const NotificationDatePicker = () => {
         <label htmlFor="phoneFromDay">Ден:</label>
         <select id="phoneFromDay" value={selectedDays.phoneFromDay} onChange={handleDayChange}>
           <option value={selectedDays.phoneFromDay}>Избери</option>
-          {renderOptions('phone')}
+          {renderOptions('phoneFrom')}
         </select>
         <input value={selectedDays.phoneFromDay}/>
       </span>
@@ -80,30 +97,30 @@ const NotificationDatePicker = () => {
         <label htmlFor="phoneToDay">Ден:</label>
         <select id="phoneToDay" value={selectedDays.phoneToDay} onChange={handleDayChange}>
           <option value="">Избери</option>
-          {renderOptions('phone')}
+          {renderOptions('phoneTo')}
         </select>
         <input value={selectedDays.phoneToDay}/>
       </span>
-
+ 
       <h2>Известие по имейл</h2>
       <span>
         <label htmlFor="emailFromDay">Ден:</label>
         <select id="emailFromDay" value={selectedDays.emailFromDay} onChange={handleDayChange}>
           <option value="">Избери</option>
-          {renderOptions('email')}
+          {renderOptions('emailFrom')}
         </select>
         <input value={selectedDays.emailFromDay}/>
       </span>
-
+ 
       <span>
         <label htmlFor="emailToDay">Ден:</label>
         <select id="emailToDay" value={selectedDays.emailToDay} onChange={handleDayChange}>
           <option value="">Избери</option>
-          {renderOptions('email')}
+          {renderOptions('emailTo')}
         </select>
         <input value={selectedDays.emailToDay}/>
       </span>
-
+ 
       <h2>Известие в приложението на телефона</h2>
       <span>
         <label htmlFor="appFromDay">Ден:</label>
@@ -113,7 +130,7 @@ const NotificationDatePicker = () => {
         </select>
         <input value={selectedDays.appFromDay}/>
       </span>
-
+ 
       <span>
         <label htmlFor="appToDay">Ден:</label>
         <select id="appToDay" value={selectedDays.appToDay} onChange={handleDayChange}>
